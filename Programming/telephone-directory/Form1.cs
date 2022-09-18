b07613c9f4878f02580c6a13a7b303e9b18c12dd
Petr Sedelnikov <petiyase@gmail.com>
2022-09-18 23:44:05 +0700

    Create Form1.cs

---
+++ Programming/telephone-directory/Form1.cs
@@ -0,0 +1,20 @@
+namespace Sedelnikov_0._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TaskLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
